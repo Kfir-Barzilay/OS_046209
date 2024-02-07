09d8ed2428a4716740be2777fee5fcb2849afa05
Kfir-Barzilay <kfir1602@gmail.com>
2024-02-07 18:41:03 +0200

    Co-authored-by: Royzou1 <Royzou1@users.noreply.github.com>

---
+++ Wet1/command.cpp
@@ -0,0 +1,159 @@
+
+#include "commands.h"
+using namespace std;
+
+
+
+//********************************************
+// function name: ExeCmd
+// Description: interperts and executes built-in commands
+// Parameters: pointer to jobs, command string
+// Returns: 0 - success,1 - failure
+//********************************************************
+int ExeCmd(void* jobs, char* lineSize, char* cmdString)
+{
+	char* cmd; 
+	char* args[MAX_ARG];
+	char pwd[MAX_LINE_SIZE];
+	char* delimiters = " \t\n";  
+	int i = 0, num_arg = 0;
+	bool illegal_cmd = FALSE; // illegal command
+    	cmd = strtok(lineSize, delimiters);
+	if (cmd == NULL)
+		return 0; 
+   	args[0] = cmd;
+	for (i=1; i<MAX_ARG; i++)
+	{
+		args[i] = strtok(NULL, delimiters); 
+		if (args[i] != NULL) 
+			num_arg++; 
+ 
+	}
+/*************************************************/
+// Built in Commands PLEASE NOTE NOT ALL REQUIRED
+// ARE IN THIS CHAIN OF IF COMMANDS. PLEASE ADD
+// MORE IF STATEMENTS AS REQUIRED
+/*************************************************/
+	if (!strcmp(cmd, "cd") ) 
+	{
+		if(num_arg > 1)//too much arguments passed
+        {
+            cout << "cd: too many arguments" ;
+            return 1;
+        }
+        if (args[1] == '-')//depends on how the struct will be constracted
+        {
+            
+        }
+        return chdir(arg[1]);
+	} 
+	/*************************************************/
+	else if (!strcmp(cmd, "pwd")) 
+	{
+		cout << getcwd();
+	}
+	/*************************************************/
+	else if (!strcmp(cmd, "jobs")) 
+	{
+ 		
+	}
+	/*************************************************/
+	else if (!strcmp(cmd, "showpid")) 
+	{
+		cout << "smash pid is " << getpid()
+	}
+	/*************************************************/
+	else if (!strcmp(cmd, "fg")) 
+	{
+		
+	} 
+	/*************************************************/
+	else if (!strcmp(cmd, "bg")) 
+	{
+  		
+	}
+	/*************************************************/
+	else if (!strcmp(cmd, "quit"))
+	{
+   		
+	} 
+	/*************************************************/
+	else if (!strcmp(cmd, "kill"))
+	{
+   		
+	} 
+	/*************************************************/	
+	else if (!strcmp(cmd, "diff"))
+	{
+   		
+	} 
+	/*************************************************/	
+	else // external command
+	{
+ 		ExeExternal(args, cmdString);
+	 	return 0;
+	}
+	if (illegal_cmd == TRUE)
+	{
+		printf("smash error: > \"%s\"\n", cmdString);
+		return 1;
+	}
+    return 0;
+}
+void ExeExternal(char *args[MAX_ARG], char* cmdString)
+{
+	int pID;
+    	switch(pID = fork()) 
+	{
+    		case -1: 
+					// Add your code here (error)
+					
+					/* 
+					your code
+					*/
+        	case 0 :
+                	// Child Process
+               		setpgrp();
+					
+			        // Add your code here (execute an external command)
+					
+					/* 
+					your code
+					*/
+			
+			default:
+                	// Add your code here
+					
+					/* 
+					your code
+					*/
+	}
+}
+//**************************************************************************************
+// function name: BgCmd
+// Description: if command is in background, insert the command to jobs
+// Parameters: command string, pointer to jobs
+// Returns: 0- BG command -1- if not
+//**************************************************************************************
+int BgCmd(char* lineSize, void* jobs)
+{
+
+	char* Command;
+	char* delimiters = " \t\n";
+	char *args[MAX_ARG];
+	if (lineSize[strlen(lineSize)-2] == '&')
+	{
+		lineSize[strlen(lineSize)-2] = '\0';
+		// Add your code here (execute a in the background)
+					
+		/* 
+		your code
+		*/
+		
+	}
+	return -1;
+}
+
+
+
+int find_command(string command, )
